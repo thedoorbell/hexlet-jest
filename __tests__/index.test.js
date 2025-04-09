48cc50bd17dc6664d194ba8bd57f060b346d0043
Jerry <mcgerbi4@gmail.com>
2025-04-09 22:26:45 +0300

    Add fixtures and rewrite tests for index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,20 @@
 import reverse from '../src/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
+  const text = readFixtureFile('text.txt').trim();
+  const result = readFixtureFile('result.txt').trim();
+
+  expect(reverse(text)).toEqual(result);
+
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
 });
